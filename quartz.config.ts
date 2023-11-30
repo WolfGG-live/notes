362547de990ed792cbeab2e75979d209f45fb475
latuh <latitud@duck.com>
2023-11-30 23:36:01 +0100

    testing

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wolfgg-live.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
